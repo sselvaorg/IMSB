f809a5728a2f35124f8f7daef5be01acea47723a
oussamahdidou <oussamahdidou15@gmail.com>
2024-09-12 14:40:02 +0100

    dashboard

--- api/src/main/java/com/example/demo/DemoApplication.java
+++ api/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @OpenAPIDefinition(info = @Info(title = "SpringBootAPiDocs", version = "1.0.0", description = "Api Documentation", contact = @Contact(name = "Oussama Hdidou", url = "https://oussamahdidou.vercel.app/#contacts")))
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
